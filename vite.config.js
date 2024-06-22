e7ecb2b3e9623ee6476264f8edf075f6534233b5
espinozakaterin <katherineaguilar575@gmail.com>
2024-06-21 20:16:15 -0600

    Chore: added Base attribute for Github pages on vite.config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(
     {
         base: '/',
         appType: 'mpa',
-        base: "/Portales2_portafolio/",
+        base: "/portalesw2_katerin/",
         build: {
             rollupOptions: {
                 input: Object.fromEntries(
